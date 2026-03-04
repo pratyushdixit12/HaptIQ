72475b20bf7295276837a62c4cc85cc12c177766
pratyushdixit12 <147138129+pratyushdixit12@users.noreply.github.com>
2026-03-05 00:22:16 +0530

    DS Store Changes

---
+++ HaptIQTests/HaptIQTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaptIQTests.swift
+//  HaptIQTests
+//
+//  Created by Shreyansh on 29/10/25.
+//
+
+import Testing
+@testable import HaptIQ
+
+struct HaptIQTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
